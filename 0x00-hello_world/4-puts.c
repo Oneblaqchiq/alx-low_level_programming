6488444f983c28d6d5fcef4f50f77e8238a3b7da
Hadassah Ibrahim <hadassahibrahim.buzz@gmail.com>
2022-09-08 14:40:04 -0700

    Low-level code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-/*
- *   main - Entry point
- *   Return: always 0
- */
+/**
+ *
+ *  *main - Entry point
+ *
+ *   *Return: Always 0 (Success)
+ *
+ *    */
 
 int main(void)
 
