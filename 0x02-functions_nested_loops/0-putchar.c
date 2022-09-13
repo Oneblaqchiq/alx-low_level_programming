5243ed9451ee38b9176cdab1c6d7359971bcdad5
Hadassah Ibrahim <hadassahibrahim.buzz@gmail.com>
2022-09-13 09:59:02 -0700

    Low level

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "_putchar"
+
+/**
+ *   main - entry block
+ *   @void: no argument
+ *   Return: 0
+*/
+
+int main(void)
+
+{
+
+	holberton[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 9; c++)
+
+	{
+
+		_putchar(holberton[c]);
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
