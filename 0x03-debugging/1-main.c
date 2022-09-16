bcbac094aa429b87e338cc1c1a831bd803d2189b
Hadassah Ibrahim <hadassahibrahim.buzz@gmail.com>
2022-09-15 19:54:26 -0700

    Low level

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,14 +11,13 @@ int main(void)
 	/* printf - show out */
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-
 	/*
 	* while (i < 10)
 	*	}
 	*		putchar(i);
 	*	}
-	* printf - Show finished out
 	*/
+	/*printf - Show finished out */
 
 	printf("Infinite loop avoided! \\o/\n");
 
