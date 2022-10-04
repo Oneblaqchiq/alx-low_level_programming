c50c28bf02f0260f77d368c751dbac4222e92e94
Hadassah Ibrahim <hadassahibrahim.buzz@gmail.com>
2022-10-04 05:32:01 -0700

    It's not bragging if you can back it up

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,19 @@
 
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *ptr;
+	unsigned int y;
+	char *i;
 
-	ptr = malloc(sizeof(char) * size);
-	if (!size || !ptr)
+	if (size == 0)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ptr[i] = c;
-	return (ptr);
+	i = malloc(size * sizeof(char));
+	if (i == NULL)
+	{
+		return (NULL);
+	}
+	for (y = 0; y < size; y++)
+	{
+		i[y] = c;
+	}
+	return (i);
 }
