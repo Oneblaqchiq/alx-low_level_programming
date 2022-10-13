85aab9d27dcf68e5dd967f95209efe51c6c2295c
Hadassah Ibrahim <hadassahibrahim.buzz@gmail.com>
2022-10-13 00:44:40 -0700

    As long as you remain adaptable, you can always be a good hacker

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+
+/**
+ * op_add - Function that adds two values
+ * @a: First value
+ * @b: Second valu
+ * Return: The sum of a+b 
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Function that subtracts
+ * @a: First value
+ * @b: Second value
+ * Return: The result of the subtracted values 
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function to multiply two values
+ * @a: First valu
+ * @b: Second valu
+ * Return: The product of two values
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function that divides two values
+ * @a: First value
+ * @b: Second value
+ * Return: The result of the divided values
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function that returns the remainder of two values
+ * @a: First value
+ * @b: Second value
+ * Return: The remainder of two values
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
